fcb9ba82f5ac2ddee17d8ef09f3172350b407a33
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-10-23 23:26:47 +0300

    Add files via upload

---
+++ src/com/company/Solution.java
@@ -0,0 +1,39 @@
+package com.company;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        Human human1 = new Human();
+        human1.setAge(18);
+        human1.setName("Tom");
+        human1.getInfo();
+        Human human2 = new Human();
+        human2.setAge(25);
+        human2.setName("Bob");
+        human2.getInfo();
+    }
+}
+class Human {
+String name;
+int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int myAge) {
+        this.age = myAge;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void getInfo() {
+        System.out.println(name + ", " + age);
+    }
+}
